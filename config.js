103953ce5c88e67a267a0cee6f97ecf91537eea6
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 19:58:35 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' GLASS ANIMALS';
+export let nameSongs = [
+  'HEAT WAVES' /*1*/,
+  'YOUTH' /*2*/,
+  'BREEZEBLOCKS X TAKE A SLICE' /*3*/,
+  'THE OTHER SIDE OF PARADISE ' /*4*/,
+  'TOES' /*5*/,
+  'BLACK MAMMOTH' /*6*/,
+  'EXXUS' /*7*/,
+  'GOLDEN ANTLERS' /*8*/,
+  'COCOA HOOVES' /*9*/,
+  'TANGERINE' /*10*/,
+  'YOUR LOVE (DÉJÀ VU)' /*11*/,
+  'HAZEY' /*12*/,
+  "MAMA'S GUN" /*13*/,
+  'AGNES' /*14*/,
+  'PORK SODA' /*15*/,
+  'LIFE ITSELF' /*16*/,
+  'POPLAR ST' /*17*/,
+  'GOOEY' /*18*/,
+  'SEASON 2 EPISODE 3' /*19*/,
+  'POOLS' /*20*/
+];
